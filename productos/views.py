0f5b3e63bf59e007cf8f5e06dafd4ddf6e0a8013
RcTorr <rictor@cuhrt.com>
2016-04-01 14:30:07 -0600

    Mostrando la info de la base de datos en el template

--- productos/views.py
+++ productos/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Producto
 
 def producto_list(request):
-    return render(request, 'productos/producto_list.html', {})
+    productos = Producto.objects.filter(fecha_registro__lte=timezone.now()).order_by('fecha_registro')
+    return render(request, 'productos/producto_list.html', {'productos': productos})
